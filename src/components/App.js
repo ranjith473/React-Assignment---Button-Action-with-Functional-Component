96c6ed76c582a5b2869770f0284e6925f1d27a2e
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2020-12-18 21:45:52 +0530

    coded button

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+			isparashown: false,
+		};
+		this.showpara = this.showpara.bind(this);
+	}
+
+	//showpara = () => {
+	showpara() {
+		this.setState({isparashown: true});
+	};
+
+    render() {
+    	return(
+    		<div id="main">
+				<button id="click" onClick={this.showpara}>
+					show paragraph
+				</button>
+				{
+					this.state.isparashown ? (<p id="para">Hello, I've learnt to use the full-stack evaluation tool.
+					 This makes me so happy</p>) : null
+				}
+				 
+    		</div>
+    	);
+    }
 }
 
 
